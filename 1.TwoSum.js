80b17e8158cf0b06fbc49ad916e8fb739c69c15f
KDubicki <kamil3234@gmail.com>
2021-12-20 22:16:38 +0100

    Palindrome Number - JS

---
+++ 1.TwoSum.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+//Better Runtime
+const twoSum = (nums, target) => {
+    length = nums.length
+
+    for (let i = 0; i < length - 1; i++) {
+        for (let j = i + 1; j < length; j++) {
+            if (nums[i] + nums[j] == target) return [i, j]
+        }
+    }
+}
+
+// const twoSum = (nums, target) => {
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums.includes(target - nums[i])) {
+//             const newIndex = nums.indexOf(target - nums[i])
+//             if (i != newIndex) {
+//                 return i < newIndex ? [i, newIndex] : [newIndex, i]
+
+//             }
+//         }
+//     }
+// }
+
+//Tests
+//     Example 1:
+//
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+//
+//     Example 2:
+//
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+//
+//     Example 3:
+//
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
---
+++ 9.PalindromeNumber.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = x => {
+    x = x.toString()
+
+    const length = x.length
+    for (let i = 0; i < length / 2; i++) {
+        if (x[i] != x[length - i - 1]) return false
+    }
+
+    return true
+}
+
+//Tests
+
+//     Example 1:
+//
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+//
+//     Example 2:
+//
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+//
+//     Example 3:
+//
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
