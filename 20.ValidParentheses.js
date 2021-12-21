1cf3ad14fa9ac1df3a534c92db4132d052519f08
KDubicki <kamil3234@gmail.com>
2021-12-21 18:09:58 +0100

    Valid Parentheses - JS

---
+++ 20.ValidParentheses.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = s => {
+    if (s.length % 2 === 1) return false
+
+    const brackets = {
+        'open': ['(', '{', '['],
+        'close': [')', '}', ']'],
+    }
+
+    const stack = []
+
+    for (let i = 0; i < s.length; i++) {
+        if (brackets.open.includes(s[i])) {
+            const openBracketIndex = brackets.open.indexOf(s[i])
+            stack.push(brackets.close[openBracketIndex])
+            continue;
+        }
+
+        if (s[i] !== stack[stack.length - 1]) return false
+        else stack.pop()
+    }
+
+    return stack.length === 0
+}
+
+/**
+ * @Input: s = "()"
+ * @Output: true
+ */
+console.log(isValid("()"))
+
+/**
+ * @Input: s = "()[]{}"
+ * @Output: true
+ */
+console.log(isValid("()[]{}"))
+
+/**
+ * @Input: s = "(]"
+ * @Output: false
+ */
+console.log(isValid("C]"))
+
+
+
